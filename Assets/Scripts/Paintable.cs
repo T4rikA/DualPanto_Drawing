36531e2af43b08bb46a8ef4077d8ac125940c431
Tarik <talnawa@outlook.de>
2020-06-29 12:45:03 +0200

    Quick-fix for drawing

--- Assets/Scripts/Paintable.cs
+++ Assets/Scripts/Paintable.cs
@@ -30,30 +30,4 @@ public class Paintable : MonoBehaviour
 
         }
 	}
-
-    public void Save()
-    {
-        StartCoroutine(CoSave());
-    }
-
-    private IEnumerator CoSave()
-    {
-        //wait for rendering
-        yield return new WaitForEndOfFrame();
-        Debug.Log(Application.dataPath + "/savedImage.png");
-
-        //set active texture
-        RenderTexture.active = RTexture;
-
-        //convert rendering texture to texture2D
-        var texture2D = new Texture2D(RTexture.width, RTexture.height);
-        texture2D.ReadPixels(new Rect(0, 0, RTexture.width, RTexture.height), 0, 0);
-        texture2D.Apply();
-
-        //write data to file
-        var data = texture2D.EncodeToPNG();
-        File.WriteAllBytes(Application.dataPath + "/savedImage.png", data);
-
-
-    }
 }
