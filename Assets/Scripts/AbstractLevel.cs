a965dcb2f413583ce20ccdb6c739833965caaff7
Paula-Kli <paula.klinke@student.hpi.de>
2020-07-13 15:56:32 +0200

    Restrict drawing area more

---
+++ Assets/Scripts/AbstractLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class AbstractLevel : MonoBehaviour
+{
+    public bool finishedDrawing;
+
+    
+
+}
