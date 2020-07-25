252a911e049dd7f852f7fa48e56269141425a61d
Paula-Kli <paula.klinke@student.hpi.de>
2020-07-25 11:43:17 +0200

    Add instructions in gameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,15 @@ using System.Linq;
 using UnityEngine.SceneManagement;
 using DualPantoFramework;
 
+/*
+This is a drawing application for the DualPanto.
+Tick "mouse" in the lineDraw Component of the Panto Object to be able to easily draw with your mouse.
+You will start at Level 1 to learn how to use the drawing App.
+Go to Level 6 to have just a short introduction and immediatly start drawing. You can do that by writing a 6 in the Main Scene in the GameManager Component.
+Disable the levelmode to be able to freely draw in case that you already know how to use the app.
+Have fun drawing.
+*/
+
 namespace PantoDrawing
 {
     public class GameManager : MonoBehaviour
